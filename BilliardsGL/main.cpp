38b2ae7ad18f2b8a629ae86d6cf059b12e4ab4ea
HSKKOU <hskk1120551@gmail.com>
2016-09-15 16:20:24 +0900

    Initial Commit

---
+++ BilliardsGL/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  BilliardsGL
+//
+//  Created by 比佐 幸基 on 2016/09/15.
+//  Copyright © 2016年 比佐 幸基. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+  // insert code here...
+  std::cout << "Hello, World!\n";
+    return 0;
+}
