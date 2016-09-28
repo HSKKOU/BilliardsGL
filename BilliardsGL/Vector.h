42be18820a1b6de29f32583cfc0b1c5853394df8
HSKKOU <hskk1120551@gmail.com>
2016-09-28 16:24:08 +0900

    add function for Vector3

--- BilliardsGL/Vector.h
+++ BilliardsGL/Vector.h
@@ -23,12 +23,18 @@ union Vector3D {
   GLfloat v[3];
   
 public:
-  Vector3D(GLfloat _x, GLfloat _y, GLfloat _z) : x(_x), y(_y), z(_z) { /* do nothing */ }
+  Vector3D(const GLfloat _x, const GLfloat _y, const GLfloat _z) : x(_x), y(_y), z(_z) { /* do nothing */ }
   
-  Vector3D operator+(GLfloat add) { return Vector3D(x+add, y+add, z+add); }
-  Vector3D operator-(GLfloat sub) { return Vector3D(x-sub, y-sub, z-sub); }
-  Vector3D operator*(GLfloat mul) { return Vector3D(x*mul, y*mul, z*mul); }
-  Vector3D operator/(GLfloat div) { return Vector3D(x/div, y/div, z/div); }
+  Vector3D operator+(const GLfloat add) { return Vector3D(x+add, y+add, z+add); }
+  Vector3D operator-(const GLfloat sub) { return Vector3D(x-sub, y-sub, z-sub); }
+  Vector3D operator*(const GLfloat mul) { return Vector3D(x*mul, y*mul, z*mul); }
+  Vector3D operator/(const GLfloat div) { return Vector3D(x/div, y/div, z/div); }
+  
+  GLfloat dot(const Vector3D v) { return x*v.x+y*v.y+z*v.z; }
+  Vector3D cross(const Vector3D v) { return Vector3D(y*v.z-z*v.y, z*v.x-x*v.z, x*v.y-y-v.x); }
+  
+  GLfloat length() { return (GLfloat)sqrt(x*x+y*y+z*z); }
+  Vector3D normalize() { return this->operator/(length()); }
   
   glm::vec3 toVec3() const { return glm::vec3(x,y,z); }
   GLfloat* toGLF3() { return v; }
@@ -52,13 +58,13 @@ union Vector4D {
   GLfloat v[4];
   
 public:
-  Vector4D(GLfloat _x, GLfloat _y, GLfloat _z, GLfloat _w) : x(_x), y(_y), z(_z), w(_w) { /* do nothing */ }
-  Vector4D(Vector3D _v3, GLfloat _w) : x(_v3.x), y(_v3.y), z(_v3.z), w(_w) { /* do nothing */ }
+  Vector4D(const GLfloat _x, const GLfloat _y, const GLfloat _z, const GLfloat _w) : x(_x), y(_y), z(_z), w(_w) { /* do nothing */ }
+  Vector4D(const Vector3D _v3, const GLfloat _w) : x(_v3.x), y(_v3.y), z(_v3.z), w(_w) { /* do nothing */ }
   
-  Vector4D operator+(GLfloat add) { return Vector4D(x+add, y+add, z+add, w+add); }
-  Vector4D operator-(GLfloat sub) { return Vector4D(x-sub, y-sub, z-sub, w-sub); }
-  Vector4D operator*(GLfloat mul) { return Vector4D(x*mul, y*mul, z*mul, w*mul); }
-  Vector4D operator/(GLfloat div) { return Vector4D(x/div, y/div, z/div, w/div); }
+  Vector4D operator+(const GLfloat add) { return Vector4D(x+add, y+add, z+add, w+add); }
+  Vector4D operator-(const GLfloat sub) { return Vector4D(x-sub, y-sub, z-sub, w-sub); }
+  Vector4D operator*(const GLfloat mul) { return Vector4D(x*mul, y*mul, z*mul, w*mul); }
+  Vector4D operator/(const GLfloat div) { return Vector4D(x/div, y/div, z/div, w/div); }
   
   glm::vec4 toVec4() const { return glm::vec4(x,y,z,w); }
   GLfloat* toGLF4() { return v; }
