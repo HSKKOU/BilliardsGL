34fe66848818699a7d28d1496905de06c84acdf3
HSKKOU <hskk1120551@gmail.com>
2016-10-06 16:13:29 +0900

    Main Camera transform fixed

--- BilliardsGL/CameraManager.cpp
+++ BilliardsGL/CameraManager.cpp
@@ -12,7 +12,7 @@ CameraManager::CameraManager() { /* do nothing */ }
 
 void CameraManager::initialize() {
   // set main camera
-  CameraController *camera0 = new CameraController(Vector3D(0.1f,0.2f,10.3f), Vector3D(0.0f,0.0f,0.0f), Vector3D(0.7f,1.8f,0.9f));
+  CameraController *camera0 = new CameraController(Vector3D(0.0f,0.0f,10.0f), Vector3D(0.0f,0.0f,0.0f), Vector3D(0.0f,1.0f,0.0f));
   addCamera(camera0);
 }
 
