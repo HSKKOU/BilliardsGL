f77faddca24071de692985ba3ac572de50f97348
HSKKOU <hskk1120551@gmail.com>
2016-10-07 17:16:53 +0900

    create Sphere class

---
+++ BilliardsGL/Sphere.cpp
@@ -0,0 +1,9 @@
+//
+//  Sphere.cpp
+//  BilliardsGL
+//
+//  Created by 比佐 幸基 on 2016/10/07.
+//  Copyright © 2016年 比佐 幸基. All rights reserved.
+//
+
+#include "Sphere.hpp"
---
+++ BilliardsGL/Sphere.hpp
@@ -0,0 +1,14 @@
+//
+//  Sphere.hpp
+//  BilliardsGL
+//
+//  Created by 比佐 幸基 on 2016/10/07.
+//  Copyright © 2016年 比佐 幸基. All rights reserved.
+//
+
+#ifndef Sphere_hpp
+#define Sphere_hpp
+
+#include <stdio.h>
+
+#endif /* Sphere_hpp */
