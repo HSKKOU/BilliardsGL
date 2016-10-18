a552bd0733abc35da75248a92b14cd106378518c
HSKKOU <hskk1120551@gmail.com>
2016-10-18 21:37:32 +0900

    change main camera status

--- BilliardsGL/GameManager.cpp
+++ BilliardsGL/GameManager.cpp
@@ -58,8 +58,8 @@ void GameManager::startMainLoop() {
   textureLoader.initialize();
   
   // set main camera
-  PerspectiveCameraController *mainCamera = new PerspectiveCameraController(Vector3D(0.0f,50.0f,0.0f), Vector3D(0.0f,0.0f,0.0f), Vector3D(0.0f,0.0f,-1.0f));
-  mainCamera->setPerspective(45.0f, 1.0f, 0.5f, 100.0f);
+  PerspectiveCameraController *mainCamera = new PerspectiveCameraController(Vector3D(0.0f,30.0f,0.0f), Vector3D(0.0f,0.0f,0.0f), Vector3D(0.0f,0.0f,-1.0f));
+  mainCamera->setPerspective(60.0f, 1.0f, 0.5f, 100.0f);
   cameraManager.addCamera(mainCamera);
   
   
@@ -89,8 +89,6 @@ void GameManager::startMainLoop() {
     balls[i]->setObjectColor(Color::one());
     float row = ceilf((sqrtf(1.0 + 8.0*i) - 1.0f) / 2.0f);
     int sumAtLastRow = (int)((row*(row-1))/2.0f);
-    int sumAtThisRow = (int)((row*(row+1))/2.0f);
-    int numOfThisRow = sumAtThisRow - sumAtLastRow;
     float posX = 1.01f * (2.0f*(i-sumAtLastRow)-row-1);
     float posZ = (row-1) * 1.75033f;
     balls[i]->translate(Vector3D(posX, 0.0f, -posZ));
@@ -112,6 +110,16 @@ void GameManager::mainLoop() {
   // Managers update
   lightManager.updateLights();
   
+  balls[0]->translate(Vector3D(0.0f, 0.0f, 10.0f-sinf(glfwGetTime())*3.0f));
+  for (int i=1; i<=15; i++) {
+    float row = ceilf((sqrtf(1.0 + 8.0*i) - 1.0f) / 2.0f);
+    int sumAtLastRow = (int)((row*(row-1))/2.0f);
+    float posX = 1.01f * (2.0f*(i-sumAtLastRow)-row-1);
+    float posZ = (row-1) * 1.75033f + sinf(glfwGetTime())*3.0f;
+    balls[i]->translate(Vector3D(posX, 0.0f, -posZ));
+    balls[i]->rotate(Quaternion(Vector3D(1.0f, 0.0f, 0.0f).normalize(), -M_PI/2.0f));
+  }
+
   objectManager.updateObject();
 
   // finish this frame, change another drawing buffer
