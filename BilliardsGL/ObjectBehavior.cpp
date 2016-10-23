ed7de11002d4fe459cf1599ad82ae71f6b5c9b0b
HSKKOU <hskk1120551@gmail.com>
2016-10-23 10:52:48 +0900

    create ObjectBehavior

---
+++ BilliardsGL/ObjectBehavior.cpp
@@ -0,0 +1,21 @@
+//
+//  ObjectBehavior.cpp
+//  BilliardsGL
+//
+//  Created by 比佐 幸基 on 2016/10/23.
+//  Copyright © 2016年 比佐 幸基. All rights reserved.
+//
+
+#include "ObjectBehavior.hpp"
+
+NS_ENGINE
+
+ObjectBehavior::ObjectBehavior() { /* do nothing */ }
+ObjectBehavior::~ObjectBehavior() { /* do nothing */ }
+
+void ObjectBehavior::awake() { /* do nothing */ }
+void ObjectBehavior::start() { /* do nothing */ }
+void ObjectBehavior::update() { /* do nothing */ }
+void ObjectBehavior::lateUpdate() { /* do nothing */ }
+
+NS_END
---
+++ BilliardsGL/ObjectBehavior.hpp
@@ -0,0 +1,30 @@
+//
+//  ObjectBehavior.hpp
+//  BilliardsGL
+//
+//  Created by 比佐 幸基 on 2016/10/23.
+//  Copyright © 2016年 比佐 幸基. All rights reserved.
+//
+
+#ifndef ObjectBehavior_hpp
+#define ObjectBehavior_hpp
+
+#include "GlobalHeader.h"
+
+NS_ENGINE
+
+class ObjectBehavior {
+  
+public:
+  ObjectBehavior();
+  virtual ~ObjectBehavior();
+  
+  void virtual awake();
+  void virtual start();
+  void virtual update();
+  void virtual lateUpdate();
+};
+
+NS_END
+
+#endif /* ObjectBehavior_hpp */
