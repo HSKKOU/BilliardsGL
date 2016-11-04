458fc9d3e66ac589a75c23042aff84cf9daac1ff
HSKKOU <hskk1120551@gmail.com>
2016-11-04 17:53:34 +0900

    impl update transform not only models but also own object

--- BilliardsGL/BaseObject3D.cpp
+++ BilliardsGL/BaseObject3D.cpp
@@ -30,23 +30,28 @@ void BaseObject3D::destroy() { ObjectBehavior::destroy(); }
 
 
 void BaseObject3D::setTransform(Engine::Transform trsf) {
+  Base3D::setTransform(trsf);
   for (BaseModel3D* model : modelList) { model->setTransform(trsf); }
 }
 
 
 void BaseObject3D::translate(Vector3D dest) {
+  Base3D::translate(dest);
   for (BaseModel3D* model : modelList) { model->translate(dest); }
 }
 
 void BaseObject3D::rotation(Quaternion rot) {
+  Base3D::rotation(rot);
   for (BaseModel3D* model : modelList) { model->rotation(rot); }
 }
 
 void BaseObject3D::scale(Vector3D scl) {
+  Base3D::scale(scl);
   for (BaseModel3D* model : modelList) { model->scale(scl); }
 }
 
 void BaseObject3D::rotate(Quaternion rot) {
+  Base3D::rotate(rot);
   for (BaseModel3D* model : modelList) { model->rotate(rot); }
 }
 
