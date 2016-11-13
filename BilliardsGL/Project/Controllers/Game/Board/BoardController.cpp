c84cb44155914a6a5cbb01d4b23d1786df43fe65
HSKKOU <hskk1120551@gmail.com>
2016-11-13 15:26:44 +0900

    create BoardController class

---
+++ BilliardsGL/Project/Controllers/Game/Board/BoardController.cpp
@@ -0,0 +1,9 @@
+//
+//  BoardController.cpp
+//  BilliardsGL
+//
+//  Created by 比佐 幸基 on 2016/11/13.
+//  Copyright © 2016年 比佐 幸基. All rights reserved.
+//
+
+#include "BoardController.hpp"
---
+++ BilliardsGL/Project/Controllers/Game/Board/BoardController.hpp
@@ -0,0 +1,14 @@
+//
+//  BoardController.hpp
+//  BilliardsGL
+//
+//  Created by 比佐 幸基 on 2016/11/13.
+//  Copyright © 2016年 比佐 幸基. All rights reserved.
+//
+
+#ifndef BoardController_hpp
+#define BoardController_hpp
+
+#include <stdio.h>
+
+#endif /* BoardController_hpp */
